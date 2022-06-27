ace80763b5163bef6d930c6b29844529eaa051ce
threecreepio <me@creep.io>
2022-06-27 21:40:28 +0200

    iNES change

--- ines.asm
+++ ines.asm
@@ -1,8 +1,13 @@
 .segment "INES"
-    .byte "NES",$1A
-    .byte 2  ; PRG SIZE
-    .byte 4  ; CHR SIZE
-    .byte $30 ; flags 6
+.byte "NES",$1A
+.byte 2   ; PRG SIZE
+.byte 4   ; CHR SIZE
+.byte $30
+.byte $8
+.byte $20
+.byte $00
+.byte $0A
+.byte $09
 
 .segment "PRG"
 .include "main.asm"
